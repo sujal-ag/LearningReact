b18b839061cc9cdb6704c038bb93198381b6fe71
Sujal Agarwal <sujalagarwal0987@gmail.com>
2025-06-17 14:02:33 +0530

    config file, appwrite setup and .env

---
+++ 10MegaProject/src/config/config.js
@@ -0,0 +1,12 @@
+//Production Grade Approach
+// Centralizes all environment variables to improve maintainability, scalability, and code cleanliness.
+
+const config = {
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+  databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+  collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+  bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+};
+
+export default config;
